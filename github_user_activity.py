83228b7945762d925b209124d3eb8dbc527418e2
Akhilesh <akhileshakhilesh2880@gmail.com>
2026-07-14 16:44:39 +0530

    Entire code ia almost done only priniting remainign

--- github_user_activity.py
+++ github_user_activity.py
@@ -1,5 +1,30 @@
 import requests
+import argparse
 
-r = requests.get('https://api.github.com/events')
+def github_activity():
 
-print(r.json())
+    parser = argparse.ArgumentParser(description="Github User Activity CLI")
+    
+    parser.add_argument("username")
+
+    args = parser.parse_args()
+
+    github_username = args.username
+    url = f"https://api.github.com/users/{github_username}/events"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        for dat in data:
+            print(f"{dat['actor']['login']}")
+            print(f"{dat['type']}")
+            print(f"{dat['repo']['name']}")
+            print(f"{dat['created_at']}")
+        
+    elif response.status_code == 404:
+        print("Error 404")
+
+    else:
+        print(":( You encounter some issue)")
+
+github_activity()
