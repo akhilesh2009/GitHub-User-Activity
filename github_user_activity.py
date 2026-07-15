c2eaaacae0c1b0af1c04499d64cae2faa7e14af1
Akhilesh <akhileshakhilesh2880@gmail.com>
2026-07-15 14:54:23 +0530

    roadmap.sh/projects 'Github user activity' project completed

--- github_user_activity.py
+++ github_user_activity.py
@@ -16,15 +16,37 @@ def github_activity():
     if response.status_code == 200:
         data = response.json()
         for dat in data:
-            print(f"{dat['actor']['login']}")
-            print(f"{dat['type']}")
-            print(f"{dat['repo']['name']}")
-            print(f"{dat['created_at']}")
+            if dat["type"] == "PushEvent":
+                print(f"Pushed commits in {dat['repo']['name']}")
+
+            elif dat["type"] == "CreateEvent":
+                print(f"Create event in {dat['repo']['name']}")
+
+            elif dat["type"] == "DeleteEvent":
+                print(f"Delete event in {dat['repo']['name']}")
+
+            elif dat["type"] == "WatchEvent":
+                print(f"Watch event in {dat['repo']['name']}")
+
+            elif dat["type"] == "ForkEvent":
+                print(f"Fork event in {dat['repo']['name']}")
+
+            elif dat["type"] == "IssuesEvent":
+                print(f"Issue event in {dat['repo']['name']}")
+
+            elif dat["type"] == "IssueCommentEvent":
+                print(f"Issue comment in {dat['repo']['name']}")
+
+            elif dat["type"] == "PullRequestEvent":
+                print(f"Pull request in {dat['repo']['name']}")
+
+            elif dat["type"] == "ReleaseEvent":
+                print(f"Release event in {dat['repo']['name']}")
         
     elif response.status_code == 404:
-        print("Error 404")
+        print(f"User Not Found")
 
     else:
-        print(":( You encounter some issue)")
+        print(f"Error {response.status_code}")
 
 github_activity()
